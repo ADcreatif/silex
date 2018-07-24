8058bfe618ecd6227af0bf48cf5495e1f76d2bb6
Hoolay <hoolay@free.fr>
2018-07-24 17:02:01 +0200

    Bases - twig - namespaces - assets - pages home,contact

--- application/Controllers/Page.php
+++ application/Controllers/Page.php
@@ -7,13 +7,10 @@ use Silex\Application;
 class Page {
 
     function home(Application $app) {
-
         return $app['twig']->render('home.twig');
-
     }
 
     function contact(Application $app) {
         return $app['twig']->render('contact.twig');
-
     }
 }
